048ebd500773efded1d45fcd5b770ec149ef4e68
quannd <quan.nguyenduc@gmail.com>
2017-05-22 05:48:44 +0700

    SublimeText Project + DB Add

---
+++ DB/ci.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : AWS My SQL
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : ci
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2017-05-15 02:23:07
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
