76e1abc9912c44dbcdd7b467f07d8251938b10ec
quannd <quan.nguyenduc@gmail.com>
2017-05-28 22:19:21 +0700

    Start Get Server Infor To Display on Screen

--- application/controllers/SystemInfo.php
+++ application/controllers/SystemInfo.php
@@ -26,51 +26,81 @@ class SystemInfo extends CI_Controller {
 	}
 
 /**
+	 * Class constructor
+	 *
+	 * @return	void
+	 */
 
+	public function __construct()
+	{
+		 parent::__construct();
+		$this->load->library('common');
+	}
+
+function SaveData(){
+	$html = $this->GetServer();
+	// RetrieveData(SERVER_INFO, $html,);
+}
+/**
+GetServer Information 
 */
 	public function GetServer(){
 
-// curl -X GET 'https://api.newrelic.com/v2/servers.json' -H 'X-Api-Key:{api_key}' -i																									
+// curl -X GET 'https://api.newrelic.com/v2/servers.json' -H 'X-Api-Key:16b31a35130b0023a8d49a86551f5bb45a52bf9069bf384' -i																									
 	$curl = curl_init();
- 	$header[0] = "X-Api-Key:16b31a35130b0023a8d49a86551f5bb45a52bf9069bf384";	
- 	$header[] = "Content-Type:application/json";
+ 	$header[0] = "X-Api-Key: 16b31a35130b0023a8d49a86551f5bb45a52bf9069bf384";	
+ 	$header[] = "Content-Type: application/json";
+ 	$header[] = "Accept-Encoding: gzip, deflate";
+
  	$url = 'https://api.newrelic.com/v2/servers.json';
  	curl_setopt($curl, CURLOPT_URL, $url);
  	curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-	curl_setopt($curl, CURLOPT_POST, false);
 
-	$ret = curl_exec($curl);
-	
+	/*Error: "SSL certificate problem: unable to get local issuer certificate"
+     * XXX: This is not a "fix" for your problem, this is a work-around.  You 
+     * should fix your local CAs 
+     */
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+	curl_setopt($curl, CURLOPT_CAINFO, getcwd() . '\application\config\cert_newrelic_base64.cer');
+
+	$html = curl_exec($curl);
+	 if ($html === false) {
+        // die('error: ' . curl_error($curl));
+        $json = array();
+        $this->common->echo_result($json, -100, 'Error: ' . curl_error($curl));
+        exit();
+    }
     // curl_close($curl);
     $response = curl_getinfo($curl);
-    
-    echo json_encode($ret);
 
     if($response['http_code'] == 403){
         // Access Forbidden
-        // echo "Access Forbidden";
         return -1; 
     }else{
-    	// echo "TEST";
-        echo json_encode($ret);
-        // return $ret;
+            return $html;
     }
 
 	}
 
-	public function echo_result(&$json, $err_code, $error_message){
-	    $json['ret'] = (($err_code == 0 || $err_code == 2) ? True : False);
-	    $json['dat']['err_code'] = $err_code;
-	    $json['dat']['err_msg'] = $error_message;
-	    echo json_encode($json);
-	}  
-
-	/* Check Html format
-	if ($html === false) {
-        // die('error: ' . curl_error($curl));
-        $json = array();
-        // echo_result($json, -100, 'Error: ' . curl_error($curl));
-        echo "Error";
-        exit();
-    }*/
+    /*function RetrieveData($type, $store_id, $html) {
+	    $d = new DOMDocument();
+	    @$d->loadHTML($html);
+	    $x = new DOMXPath($d);
+	    $output = array();
+	    switch_type($type, $x, $output);
+	    if(count($output) > 0){
+	    // SAVE SERVER DATA
+	        if ($type == SERVER_INFO) {
+	            return save_data_download($store_id, $output);
+	        } else{
+	    // SAVE APPLICATION DATA
+	        // if ($type == APPLICATION_INFO) {
+	            return save_data_earning($store_id, $output);
+	        }
+	    }else{
+	        // No Data
+	        return 2;
+	    }
+	}*/
 }
