145de003518301e60dbc405e9ac175497c124c15
Nguyen Duc Quan <quan.nguyenduc@gmail.com>
2017-06-02 18:20:08 +0700

    Add update

---
+++ application/views/widget/test.php
@@ -0,0 +1,23 @@
+<!--	
+	Encoding and decoding widget
+	
+	Widget parameters 
+		widget_width_percentage from 250 to 1560 in pixels, other with 550 always
+		widget_height_pixels from 50 to 650 in pixels, other with 150 always 
+	
+	Version
+		2.0
+	
+	copyright
+		Copyright (c) 2012-2017 PhpFiddle.org
+	
+	License
+		http://opensource.org/licenses/MIT  The MIT License (MIT)
+-->
+<div id="phpfiddle_widget_003"></div>
+<script src="//phpfiddle.org/js/pf-encode-decode.min.js" type="text/javascript"></script>
+<script src="//phpfiddle.org/deposit/widget_003.php
+		?widget_width_pixels=550
+		&widget_height_pixels=150" 
+	type="text/javascript">
+</script>
